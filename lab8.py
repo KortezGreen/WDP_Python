559dde98a3cf5dc59cef4520bb3a1f782e460fe3
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2024-11-22 08:42:34 +0100

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -1,1 +1,18 @@
+print('Cw1\n')
+napis = 'Bezpłatny kalkulator znaków informujący precyzyjnie o objętości tekstu zawsze pod ręką nie musisz liczyć liter ręcznie'
+#a
+print (len(napis))
+#b
+print(napis[12])
+print(napis[7])
+print(napis[-4])
+napis2 = napis[12]+napis[7]+napis[-4]
+print(napis2)
+#c
+print(napis+napis2)
+#d
+print(napis.lower())
+#e
+print(napis.upper())
 
+print('Cw2\n')
