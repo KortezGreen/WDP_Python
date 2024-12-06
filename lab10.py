1e2dcccf5b07a9ba5c516217bb8a6183606e8a1a
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2024-12-06 09:10:39 +0100

    Create lab10.py

---
+++ lab10.py
@@ -0,0 +1,40 @@
+import math
+#(Ćwiczenie 1)
+#a
+# lista1 = [1,2,3,'a','b','c','!','@',5,7]
+# lista1[2] = 9
+# print(lista1[8])
+# print(lista1[::-2])
+# print(lista1[-4])
+#b
+# def sortowanie(lista, listaS):
+#     if listaS==lista:
+#         return True
+#     else:
+#         return False
+# lista = [1,2,3]
+# listaS = lista.sort()
+# print(sortowanie(lista,listaS))
+# print(listaS)
+#c
+# a = ['a','b','c']
+# b = [1,2,3]
+# def dwaF(a,b):
+#     return a+b
+# print(dwaF(a,b))
+#d
+# def func(lint, n1, n2):
+#     for x in lint:
+#         if == n1:
+#             lint[x]=n2
+#     return None
+# lint = [1,2,3,4,5]
+# n1=1
+# n2=8
+# print(func(lint,n1,n2))
+#g
+mies =['styczeń','luty','marzec','kwiecień','maj','czerwiec','lipiec','sierpień','wrzesień','pażdziernik','listopad','grudzień']
+mies.sort()
+a = 'luty'
+def miesF(mies,a):
+    
